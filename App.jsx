abc1f60d44efa9bda61ebb8a82b704f7e6aa5b9f
Spck Editor <kautsu@elisanet.fi>
2024-10-17 21:59:03 +0300

    init App.jsx The basic app

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dashboard from './Dashboard.jsx';
+
+/* ReactDOM.render(
+  <Dashboard classes={{
+    tableContainer: '',
+    appBarSpacer: '',
+    container: '',
+    drawerPaper: '',
+    title: '',
+    toolbar: '',
+    root: ''
+  }} />,
+  mountNode);
+  */
